d2058adbc043d84c97f34899ad7f8c8c0d81c3f7
Shresth Sharma <76861631+Shresth-Sharma@users.noreply.github.com>
2025-02-24 13:16:50 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,21 +1,89 @@
 chapterlist = {
     '11':{
-        'Maths':['Ch-1 Sets Numbers & Intervals', 'Ch-2 Fundamentals of Algebra', 'Ch-3 Quadratic Equation', 'Ch-4 Logarithm'],
-        'Physics':['Ch-1 Units&Dimension, Basic Maths and Vector', 'Ch-2 Kinematics-1D and Calculus', 'Ch-3 Kinematics-2D', 'Ch-4 Newton\'s Laws of motion & Friction'],
-        'Chemistry':['Ch-1 Quantum Number', 'Ch-2 Periodic Table', 'Ch-3 Chemical Bonding'],
-        'Biology':['Ch-1 Cell The unit of Life'],
+        'Maths':['Ch-1 Sets Numbers & Intervals', 'Ch-2 Fundamentals of Algebra', 'Ch-3 Quadratic Equation', 'Ch-4 Logarithm', 'Ch-5 Sequence and Series', 'Ch-6 Trigonometric Ratios', 'Ch-7 Trigonometric Equations', 'Ch-8 Solution Of Triangles', 'Ch-9 Determinants', 'Ch-10 Straight Line', 'Ch-11 Circles', 'Ch-12 Parabola', 'Ch-13 Ellipse and Hyperbola', 'Ch-14 Binomial Theorm', 'Ch-15 Permutation and Combination'],
+        'Physics':['Ch-1 Units&Dimension, Basic Maths and Vector', 'Ch-2 Kinematics-1D and Calculus', 'Ch-3 Kinematics-2D', 'Ch-4 Newton\'s Laws of motion & Friction', 'Ch-5 Work, Power and Energy', 'Ch-6 Circular Motion', 'Ch-7 System of Particles', 'Ch-8 Rotation', 'Ch-9 Elasticity', 'Ch-9 Calorimetry ', 'Ch-9 Thermal Expansion', 'Ch-9 Heat Transfer', 'Ch-10 Kinetic Theory Of Gases', 'Ch-11 Thermodynamics', 'Ch-12 Fluids', 'Ch-13 Simple Harmonic Motion'],
+        'Chemistry':['Ch-1 Quantum Number', 'Ch-2 Periodic Table', 'Ch-3 Chemical Bonding', 'Ch-4 Mole Concept', 'Ch-5 Concentration Terms', 'Ch-6 Redox Reaction', 'Ch-7 Ideal Gas', 'Ch-8 Real Gas', 'Ch-9 Atomic Structure', 'Ch-10 Chemical Equilibrium ', 'Ch-11 Nomenclature'],
+        'Biology':['Not Available!'],
         'English':['Not Available!']
     }
 }
 notebooklist = {
     '11':{
-        'Maths':['Notebook-1', 'Notebook-2'],
-        'Physics':['Notebook-1', 'Notebook-2'],
-        'Chemistry':['Notebook-1', 'Notebook-2'],
-        'Biology':['Notebook-1'],
+        'Maths':['Notebook-1', 'Notebook-2', 'Notebook-3', 'Notebook-4', 'Shorts', 'Complete'],
+        'Physics':['Notebook-1', 'Notebook-2', 'Notebook-3', 'Notebook-4', 'Shorts', 'Complete'],
+        'Chemistry':['Notebook-1', 'Notebook-2', 'Notebook-3', 'Notebook-4', 'Shorts', 'Complete'],
+        'Biology':['Not Available!'],
         'English':['Not Available!']
     }
 }
+booklist = {
+    '11':{
+        'Maths':['Disha PYQs(1978-2020)'],
+        'Physics':['Disha PYQs(1978-2020)', 'Irodov','Irodov-Solution-1','Irodov-Solution-2'],
+        'Chemistry':['Disha PYQs(1978-2020)'],
+        'Biology':['Not Available!'],
+        'English':['Not Available!']
+    }
+}
+chapter1list = {
+    '11':{
+        'Maths':['Ch-1 Sets, Numbers and Interval', 'Ch-2 Fundemantal Of Algebra', 'Ch-3 Quadratic Equation', 'Ch-4 Logarithm', 'Ch-5 Sequence _ Series', 'Ch-6 Trigonometric Ratios _ Identities', 'Ch-7 Trigonometric Equation', 'Ch-8 Properties of Triangle', 'Ch-9 Determinant', 'Ch-10 Straight line', 'Ch-11 Circle', 'Ch-12 Parabola', 'Ch-13 Ellipse', 'Ch-13 Hyperbola', 'Ch-14 Binomial Theorem', 'Ch-15 Permutation _ Combination'],
+        'Physics':['Ch-1 Unit _ Dimension_ Basic Maths and Vector', 'Ch-2 Kinematics-1D _ Calculus', 'Ch-3 Kinematics-2D','Ch-4 NLM','Ch-5 Work_ Power _ Energy','Ch-6 Circular Motion','Ch-7 Center of Mass_ Momentum _ Collision','Ch-8 Rotational Dynamics','Ch-9 Elasticity_ Thermal Expansion_ Calorimetry _ Heat Transfer','Ch-10 KTG _ Thermodynamics','Ch-12 Fluid Mechanics','Ch-13 SHM'],
+        'Chemistry':['Ch-1 Quantum Number','Ch-2 Periodic Table','Ch-3 Chemical Bonding','Ch-4 Mole Concept','Ch-5 Concentration Terms','Ch-6 Redox Reactions','Ch-7 Ideal Gas','Ch-8 Real Gas','Ch-9 Atomic Structure','Ch-10 Chemical equilibrium','Ch-11 Nomenclature'],
+        'Biology':['Not Available!'],
+        'English':['Not Available!']
+    }
+}
+filelist = {
+    '11': {
+        'Maths': {
+            'Ch-1 Sets, Numbers and Interval': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-2 Fundemantal Of Algebra': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-1'],
+            'Ch-3 Quadratic Equation': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-4 Logarithm': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-1'],
+            'Ch-5 Sequence _ Series': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-6 Trigonometric Ratios _ Identities': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-7 Trigonometric Equation': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-8 Properties of Triangle': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-1', 'Exercise-2'],
+            'Ch-9 Determinant': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-10 Straight line': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-11 Circle': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-12 Parabola': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-13 Ellipse': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-13 Hyperbola': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-14 Binomial Theorem': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1'],
+            'Ch-15 Permutation _ Combination': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6', 'Exercise-1']
+        },
+        'Physics': {
+            'Ch-1 Unit _ Dimension_ Basic Maths and Vector': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'Exercise-1'],
+            'Ch-2 Kinematics-1D _ Calculus': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'Exercise-2'],
+            'Ch-3 Kinematics-2D': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'Exercise-2'],
+            'Ch-4 NLM': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'Exercise-3'],
+            'Ch-5 Work_ Power _ Energy': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-6 Circular Motion': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-7 Center of Mass_ Momentum _ Collision': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-8 Rotational Dynamics': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-9 Elasticity_ Thermal Expansion_ Calorimetry _ Heat Transfer': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-10 KTG _ Thermodynamics': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-12 Fluid Mechanics': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2'],
+            'Ch-13 SHM': ['EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'Exercise-2']
+        },
+        'Chemistry': {
+            'Ch-1 Quantum Number': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-2 Periodic Table': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-3 Chemical Bonding': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-4 Mole Concept': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-5 Concentration Terms': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-6 Redox Reactions': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-7 Ideal Gas': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-8 Real Gas': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-9 Atomic Structure': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4', 'EX_SOL-5', 'EX_SOL-6'],
+            'Ch-10 Chemical equilibrium': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-6'],
+            'Ch-11 Nomenclature': ['Exercise-1', 'EX_SOL-1', 'EX_SOL-2', 'EX_SOL-3', 'EX_SOL-4']
+        }
+    }
+}
+
 let width = window.innerWidth;
 let height = window.innerHeight;
 if (width / height < 1) {
@@ -28,39 +96,105 @@ document.addEventListener("DOMContentLoaded", function() {
         document.getElementById('chdw').style.display = 'block';
         document.getElementById('nblabel').style.display = 'none';
         document.getElementById('nbdw').style.display = 'none';
+        document.getElementById('chdw1').style.display = 'none';
+        document.getElementById('fldw').style.display = 'none';
+        document.getElementById('fllabel').style.display = 'none';
+        document.getElementById('bkdw').style.display = 'none';
+        document.getElementById('bklabel').style.display = 'none';
     }
-    else{
+    else if(chno.value == 'notebook'){
         document.getElementById('chlabel').style.display = 'none';
         document.getElementById('chdw').style.display = 'none';
         document.getElementById('nblabel').style.display = 'block';
         document.getElementById('nbdw').style.display = 'block';
+        document.getElementById('chdw1').style.display = 'none';
+        document.getElementById('fldw').style.display = 'none';
+        document.getElementById('fllabel').style.display = 'none';
+        document.getElementById('bkdw').style.display = 'none';
+        document.getElementById('bklabel').style.display = 'none';
     } 
+    else if(chno.value == 'module'){
+        document.getElementById('chlabel').style.display = 'block';
+        document.getElementById('fllabel').style.display = 'block';
+        document.getElementById('nblabel').style.display = 'none';
+        document.getElementById('chdw').style.display = 'none';
+        document.getElementById('chdw1').style.display = 'block';
+        document.getElementById('fldw').style.display = 'block';
+        document.getElementById('nbdw').style.display = 'none';
+        document.getElementById('bkdw').style.display = 'none';
+        document.getElementById('bklabel').style.display = 'none';
+    }
+    else if(chno.value == 'book'){
+        document.getElementById('chlabel').style.display = 'none';
+        document.getElementById('fllabel').style.display = 'none';
+        document.getElementById('nblabel').style.display = 'none';
+        document.getElementById('chdw').style.display = 'none';
+        document.getElementById('chdw1').style.display = 'none';
+        document.getElementById('fldw').style.display = 'none';
+        document.getElementById('nbdw').style.display = 'none';
+        document.getElementById('bkdw').style.display = 'block';
+        document.getElementById('bklabel').style.display = 'block';
+    }
     chno.addEventListener('change', function() {
         if(chno.value == 'chapter'){
             document.getElementById('chlabel').style.display = 'block';
             document.getElementById('chdw').style.display = 'block';
             document.getElementById('nblabel').style.display = 'none';
             document.getElementById('nbdw').style.display = 'none';
-            updatecontent()
+            document.getElementById('chdw1').style.display = 'none';
+            document.getElementById('fldw').style.display = 'none';
+            document.getElementById('fllabel').style.display = 'none';
+            updatecontent();
         }
-        else{
+        else if(chno.value == 'notebook'){
             document.getElementById('chlabel').style.display = 'none';
             document.getElementById('chdw').style.display = 'none';
             document.getElementById('nblabel').style.display = 'block';
             document.getElementById('nbdw').style.display = 'block';
-            updatecontent()
+            document.getElementById('chdw1').style.display = 'none';
+            document.getElementById('fldw').style.display = 'none';
+            document.getElementById('fllabel').style.display = 'none';
+            updatecontent();
         } 
+        else if(chno.value == 'module'){
+            document.getElementById('chlabel').style.display = 'block';
+            document.getElementById('fllabel').style.display = 'block';
+            document.getElementById('nblabel').style.display = 'none';
+            document.getElementById('chdw').style.display = 'none';
+            document.getElementById('chdw1').style.display = 'block';
+            document.getElementById('fldw').style.display = 'block';
+            document.getElementById('nbdw').style.display = 'none';
+            updatecontent();
+        }
+        else if(chno.value == 'book'){
+            document.getElementById('chlabel').style.display = 'none';
+            document.getElementById('fllabel').style.display = 'none';
+            document.getElementById('nblabel').style.display = 'none';
+            document.getElementById('chdw').style.display = 'none';
+            document.getElementById('chdw1').style.display = 'none';
+            document.getElementById('fldw').style.display = 'none';
+            document.getElementById('nbdw').style.display = 'none';
+            document.getElementById('bkdw').style.display = 'block';
+            document.getElementById('bklabel').style.display = 'block';
+            updatecontent();
+        }
     });
 });
 classobj = document.getElementById('class')
 subjectobj = document.getElementById('subject')
 chapterobj = document.getElementById('chdw')
+chapter1obj = document.getElementById('chdw1')
 notebookobj = document.getElementById('nbdw')
+bookobj = document.getElementById('bkdw')
+fileobj = document.getElementById('fldw')
 chno = document.getElementById('ChNo');
 classval = classobj.value;
 subjectval = subjectobj.value;
 chapterval = chapterobj.value;
+chapter1val = chapter1obj.value;
 notebookval = notebookobj.value;
+bookval = bookobj.value;
+fileval = fileobj.value;
 classobj.addEventListener('change', function(){
     classval = classobj.value;
     updatecontent()
@@ -68,6 +202,10 @@ classobj.addEventListener('change', function(){
 subjectobj.addEventListener('change', function(){
     subjectval = subjectobj.value;
     updatecontent()
+})
+chapter1obj.addEventListener('change', function(){
+    chapter1val = chapter1obj.value;
+    updatecontent1()
 })
 updatecontent()
 function updatecontent(){
@@ -84,7 +222,7 @@ function updatecontent(){
             chapterobj.appendChild(option);
         }
     }
-    else{
+    else if(chno.value == 'notebook'){
         list = notebooklist[classval][subjectval]
         console.log(list)
         while (notebookobj.firstChild) {
@@ -97,21 +235,82 @@ function updatecontent(){
             notebookobj.appendChild(option);
         }
     }
+    else if(chno.value == 'book'){
+        list = booklist[classval][subjectval]
+        console.log(list)
+        while (bookobj.firstChild) {
+            bookobj.removeChild(bookobj.firstChild);
+        }
+        for (let i = 0; i < list.length; i++) {
+            let option = document.createElement('option');
+            option.value = list[i];
+            option.textContent = list[i];
+            bookobj.appendChild(option);
+        }
+    }
+    else if(chno.value == 'module'){
+        list1 = chapter1list[classval][subjectval]
+        console.log(list1)
+        while (chapter1obj.firstChild){
+            chapter1obj.removeChild(chapter1obj.firstChild);
+        }
+        for (let i = 0; i < list1.length; i++){
+            let option = document.createElement('option');
+            option.value = list1[i];
+            option.textContent = list1[i];
+            chapter1obj.appendChild(option);
+        }
+        chapter1val = chapter1obj.value;
+        list = filelist[classval][subjectval][chapter1val]
+        console.log(list)
+        while (fileobj.firstChild){
+            fileobj.removeChild(fileobj.firstChild);
+        }
+        for (let j = 0; j < list.length; j++){
+            let option = document.createElement('option');
+            option.value = list[j];
+            option.textContent = list[j];
+            fileobj.appendChild(option);
+        }
+    }
 }
-
+function updatecontent1(){
+    chapter1val = chapter1obj.value;
+    list = filelist[classval][subjectval][chapter1val]
+    console.log(list)
+    // console.log(chapter1val)
+    while (fileobj.firstChild){
+        fileobj.removeChild(fileobj.firstChild);
+        }
+        for (let j = 0; j < list.length; j++){
+            let option = document.createElement('option');
+            option.value = list[j];
+            option.textContent = list[j];
+            fileobj.appendChild(option);
+        }
+}
 document.getElementById('index').addEventListener('click', function(){
-    window.open('pdfs/indexes.pdf', '_blank')
+    window.open('https://firebasestorage.googleapis.com/v0/b/shresth-sharma-v3.appspot.com/o/notes%2F11_Index.pdf?alt=media', '_blank')
 })
 document.getElementById('fetch').addEventListener('click', function(){
     classval = classobj.value;
     subjectval = subjectobj.value;
     chapterval = chapterobj.value;
+    chapter1val = chapter1obj.value;
     notebookval = notebookobj.value;
+    bookval = bookobj.value;
+    fileval = fileobj.value;
     // alert('pdfs/' + classval + '_' + subjectval + '_' + chapterval)
     if(chno.value == 'chapter'){
-        window.open('pdfs/' + classval + '_' + subjectval + '_' + chapterval + '.pdf', '_blank')
+        window.open('https://firebasestorage.googleapis.com/v0/b/shresth-sharma-v3.appspot.com/o/notes%2F' + classval + '_' + subjectval + '_' + chapterval + '.pdf'+'?alt=media', '_blank')
     }
-    else{
-        window.open('pdfs/' + classval + '_' + subjectval + '_' + notebookval + '.pdf', '_blank')
+    else if(chno.value == 'notebook'){
+        window.open('https://firebasestorage.googleapis.com/v0/b/shresth-sharma-v3.appspot.com/o/notes%2F' + classval + '_' + subjectval + '_' + notebookval + '.pdf'+'?alt=media', '_blank')
+    }
+    else if(chno.value == 'book'){
+        window.open('https://firebasestorage.googleapis.com/v0/b/shresth-sharma-v3.appspot.com/o/notes%2F' + classval + '_' + subjectval + '_Book_' + bookval + '.pdf'+'?alt=media', '_blank')
+    }
+    else if(chno.value == 'module'){
+        window.open('https://firebasestorage.googleapis.com/v0/b/shresth-sharma-v3.appspot.com/o/notes%2F' + classval + '_' + subjectval + '_Module_' + chapter1val + '-' + fileval + '.pdf'+'?alt=media', '_blank')
     }
 })
